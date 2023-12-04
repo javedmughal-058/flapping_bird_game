34e77ec2e9b9099482783431133276dc24b6ffc2
javedmughal <javedmughal609@gmail.com>
2023-12-04 14:12:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flapping_bird_game
+ This is a flapping bird game build using flame in flutter 
---
+++ android/app/src/main/kotlin/com/example/flapping_bird_game_using_flame_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flapping_bird_game_using_flame_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
